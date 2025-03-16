2796eea59676671031dbe96558a195bffdc32a3d
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-16 14:55:44 -0600

    add

--- Imprimir_Num.c
+++ Imprimir_Num.c
@@ -3,7 +3,7 @@
 int main (){
     
     int numero; 
-    printf("Ingresa un numero:");
+    printf("Ingresa un numero: ");
     scanf("%d", &numero);
     printf("Se ingreso un %d", numero);
     return 0;
